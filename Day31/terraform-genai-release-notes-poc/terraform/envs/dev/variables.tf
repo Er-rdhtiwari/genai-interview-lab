c0504984d3fdef0563b797e4eb71348e8696cb64
Radheshyam Tiwari <radhe.tiwari@ibm.com>
2025-12-09 23:17:36 +0530

    fixed dns domain bug

--- Day31/terraform-genai-release-notes-poc/terraform/envs/dev/variables.tf
+++ Day31/terraform-genai-release-notes-poc/terraform/envs/dev/variables.tf
@@ -12,11 +12,7 @@ variable "aws_region" {
   default     = "ap-south-1"
 }
 
-variable "root_domain" {
-  type        = string
-  description = "Root domain for the hosted zone (e.g. example.com)."
-  default     = "example.com"
-}
+
 variable "db_username" {
   type        = string
   description = "Master username for the dev Postgres instance."
