0353ad4308b9816d040d77773424603bfebb3e45
Radheshyam Tiwari <radhe.tiwari@ibm.com>
2025-12-17 15:50:28 +0530

    updated eks_managed_node_groups to public

--- Day33/oss-chat/d33-oss-prod/terraform/eks-d33/eks.tf
+++ Day33/oss-chat/d33-oss-prod/terraform/eks-d33/eks.tf
@@ -9,14 +9,13 @@ module "eks" {
   enable_cluster_creator_admin_permissions = true
 
   vpc_id     = module.vpc.vpc_id
-  # before
-  # subnet_ids = module.vpc.private_subnets
-  # after (temporary)
-  subnet_ids = module.vpc.public_subnets
+  subnet_ids = module.vpc.private_subnets
+
 
   eks_managed_node_groups = {
     core = {
       ami_type       = "AL2023_x86_64_STANDARD"
+      subnet_ids     = module.vpc.public_subnets
       instance_types = ["t3.medium"]
       min_size       = 1
       max_size       = 3
@@ -26,6 +25,7 @@ module "eks" {
 
     model = {
       ami_type       = "AL2_x86_64"
+      subnet_ids     = module.vpc.public_subnets
       instance_types = ["m5.xlarge"]
       min_size       = 0
       max_size       = 2
